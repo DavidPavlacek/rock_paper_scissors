1098c61a834344e8c8ef3180e1a1427549ee0326
DavidPavlacek <dav.pavlacek@gmail.com>
2021-04-28 21:21:03 +0200

    adding computer play

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World!")
+const choice = [
+    "rock",
+    "paper",
+    "scissors"
+];
+const randomChoice = choice[Math.floor(Math.random() * choice.length)];
+
+
+function computerPlay() {
+    return (randomChoice);
+}
+
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+function playRound() {
+    console.log(playerSelection);
+    console.log(computerSelection);
+}
+console.log( playRound() );

5fc67c302aafdd4807e2da5206b09726bdc31b5a
DavidPavlacek <dav.pavlacek@gmail.com>
2021-04-30 20:13:36 +0200

    figuring out one round of the game

--- script.js
+++ script.js
@@ -11,11 +11,38 @@ function computerPlay() {
 }
 
 
-const playerSelection = "rock";
+const playerSelection = "paper";
 const computerSelection = computerPlay();
 
-function playRound() {
-    console.log(playerSelection);
-    console.log(computerSelection);
+//console.log(computerPlay());
+//console.log(playerSelection);
+
+function playRound(computerSelection, playerSelection) {
+
+    computerSelection = computerPlay();
+    playerSelection = "paper";
+    
+    if ( playerSelection === computerPlay()) {
+        console.log("Its a tie!");
+    }
+
+    else if (playerSelection === "rock" && computerPlay() === "scissors") {
+        console.log("You win. Rock crushes scissors");
+    }
+
+    else if (playerSelection === "rock" && computerPlay() === "paper") {
+        console.log("You loose. Paper wraps rock");
+    }
+
+    else if (playerSelection === "paper" && computerPlay() === "scissors") {
+        console.log("You loose. Scissors cut PAPER");
+    }
+
+    else if (playerSelection === "paper" && computerPlay() === "rock") {
+        console.log("You win. PAPER wraps rock");
+    }
+
+    else console.log("whatever");
 }
-console.log( playRound() );
+
+console.log(playRound(computerSelection, playerSelection));

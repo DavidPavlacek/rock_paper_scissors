c097e85f01a8bf79313006eecfa785a4c03c8560
DavidPavlacek <dav.pavlacek@gmail.com>
2021-04-30 20:21:31 +0200

    adding all 3 possibilites in if statements

--- script.js
+++ script.js
@@ -11,27 +11,25 @@ function computerPlay() {
 }
 
 
-const playerSelection = "paper";
+const playerSelection = "scissors";
 const computerSelection = computerPlay();
 
-//console.log(computerPlay());
-//console.log(playerSelection);
 
 function playRound(computerSelection, playerSelection) {
 
     computerSelection = computerPlay();
-    playerSelection = "paper";
+    playerSelection = "scissors";
     
     if ( playerSelection === computerPlay()) {
         console.log("Its a tie!");
     }
 
     else if (playerSelection === "rock" && computerPlay() === "scissors") {
-        console.log("You win. Rock crushes scissors");
+        console.log("You win. ROCK crushes scissors");
     }
 
     else if (playerSelection === "rock" && computerPlay() === "paper") {
-        console.log("You loose. Paper wraps rock");
+        console.log("You loose. Paper wraps ROCK");
     }
 
     else if (playerSelection === "paper" && computerPlay() === "scissors") {
@@ -42,7 +40,16 @@ function playRound(computerSelection, playerSelection) {
         console.log("You win. PAPER wraps rock");
     }
 
+
+    else if (playerSelection === "scissors" && computerPlay() === "paper") {
+        console.log("You win. SCISSORS cut paper");
+    }
+
+    else if (playerSelection === "scissors" && computerPlay() === "rock") {
+        console.log("You loose. Rock crushes SCISSORS");
+    }
+
     else console.log("whatever");
-}
+    }
 
 console.log(playRound(computerSelection, playerSelection));

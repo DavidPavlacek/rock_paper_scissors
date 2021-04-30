f2de08eb604b54f7cf910da1282849eaa07fa8b0
DavidPavlacek <dav.pavlacek@gmail.com>
2021-04-30 20:40:27 +0200

    adding .toLowerCase for player choice

--- script.js
+++ script.js
@@ -10,46 +10,42 @@ function computerPlay() {
     return (randomChoice);
 }
 
-
-const playerSelection = "scissors";
+const playerSelection = prompt("Enter rock, paper or scissors");
 const computerSelection = computerPlay();
 
 
-function playRound(computerSelection, playerSelection) {
+function playRound() {
 
-    computerSelection = computerPlay();
-    playerSelection = "scissors";
     
-    if ( playerSelection === computerPlay()) {
+    if ( playerSelection.toLowerCase() === computerPlay()) {
         console.log("Its a tie!");
     }
 
-    else if (playerSelection === "rock" && computerPlay() === "scissors") {
+    else if (playerSelection.toLowerCase() === "rock" && computerPlay() === "scissors") {
         console.log("You win. ROCK crushes scissors");
     }
 
-    else if (playerSelection === "rock" && computerPlay() === "paper") {
+    else if (playerSelection.toLowerCase() === "rock" && computerPlay() === "paper") {
         console.log("You loose. Paper wraps ROCK");
     }
 
-    else if (playerSelection === "paper" && computerPlay() === "scissors") {
+    else if (playerSelection.toLowerCase() === "paper" && computerPlay() === "scissors") {
         console.log("You loose. Scissors cut PAPER");
     }
 
-    else if (playerSelection === "paper" && computerPlay() === "rock") {
+    else if (playerSelection.toLowerCase() === "paper" && computerPlay() === "rock") {
         console.log("You win. PAPER wraps rock");
     }
 
-
-    else if (playerSelection === "scissors" && computerPlay() === "paper") {
+    else if (playerSelection.toLowerCase() === "scissors" && computerPlay() === "paper") {
         console.log("You win. SCISSORS cut paper");
     }
 
-    else if (playerSelection === "scissors" && computerPlay() === "rock") {
+    else if (playerSelection.toLowerCase() === "scissors" && computerPlay() === "rock") {
         console.log("You loose. Rock crushes SCISSORS");
     }
 
-    else console.log("whatever");
+    else console.log("Did you spell choice of your weapon correctly?");
     }
 
-console.log(playRound(computerSelection, playerSelection));
+console.log(playRound());

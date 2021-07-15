7cd0f647fee178e0f793abb6af2a15c1c104a9a3
DavidPavlacek <dav.pavlacek@gmail.com>
2021-07-15 21:08:28 +0200

    removed classList.played for user and computer choice

--- script.js
+++ script.js
@@ -13,12 +13,11 @@ let computerScore = 0;
 let round = 0;
 let gameIsActive = true;
 let computerRock = document.getElementById("rock-button-computer");
-let computerPaper = document.getElementById("paper-button-computer")
-let computerScissors = document.getElementById("scissors-button-computer")
+let computerPaper = document.getElementById("paper-button-computer");
+let computerScissors = document.getElementById("scissors-button-computer");
 let userRock = document.getElementById("rock-button");
-let userPaper = document.getElementById("paper-button")
-let userScissors = document.getElementById("scissors-button")
-
+let userPaper = document.getElementById("paper-button");
+let userScissors = document.getElementById("scissors-button");
 
 function winCondition() {
     if (playerScore >= 5) {
@@ -29,14 +28,30 @@ function winCondition() {
         document.getElementById("round-result").textContent = "OH NO! You've lost the game!"
 }};
 
-function removeTransition() {
+function removeComputerTransition() {
     computerRock.classList.remove("played");
     computerPaper.classList.remove("played");
     computerScissors.classList.remove("played");
-    userRock.classList.remove("played");
-    userPaper.classList.remove("played");
-    userScissors.classList.remove("played");    
 }
+function removeTransitionRock() {
+    userRock.addEventListener("transitionend", function(e) {
+        if (e.propertyName === "transform") {
+            userRock.classList.remove("played");
+            removeComputerTransition();
+}})};
+
+function removeTransitionPaper() {
+    userPaper.addEventListener("transitionend", function(e) {
+        if (e.propertyName === "transform") {
+            userPaper.classList.remove("played");
+            removeComputerTransition();
+}})};
+function removeTransitionScissors() {
+    userScissors.addEventListener("transitionend", function(e) {
+        if (e.propertyName === "transform") {
+            userScissors.classList.remove("played");
+            removeComputerTransition();
+}})};
 
 function playRound(playerSelection, computerSelection) {
 
@@ -45,7 +60,7 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection) {
         ++round
-        document.getElementById("round-result").textContent += "#" + round + " Its a tie! No points." + newLine 
+        document.getElementById("round-result").textContent += "#" + round + " It's a tie! No points." + newLine 
         if (playerSelection === "rock") {userRock.classList.add("played") 
                                         computerRock.classList.add("played")}
         if (playerSelection === "paper") {userPaper.classList.add("played") 
@@ -55,43 +70,37 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         ++round
         ++playerScore;
-        computerScissors.classList.add("played");
-        userRock.classList.add("played");
+        computerScissors.classList.add("played");       
         document.getElementById("user-points").textContent = playerScore;
         document.getElementById("round-result").textContent += "#" + round + " You win. ROCK crushes scissors!"  + newLine
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         ++round
         ++computerScore;
         computerPaper.classList.add("played");
-        userRock.classList.add("played");
         document.getElementById("computer-points").textContent = computerScore;
         document.getElementById("round-result").textContent += "#" + round + " You loose. Paper wraps ROCK!" + newLine
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         ++round
         ++playerScore;
-        computerRock.classList.add("played");
-        userPaper.classList.add("played");
+        computerRock.classList.add("played");       
         document.getElementById("user-points").textContent = playerScore;
         document.getElementById("round-result").textContent += "#" + round + " You win. PAPER wraps rock!" + newLine
     } else if (playerSelection  === "paper" && computerSelection === "scissors") {
         ++round
         ++computerScore;
         computerScissors.classList.add("played");
-        userPaper.classList.add("played");
         document.getElementById("computer-points").textContent = computerScore;
         document.getElementById("round-result").textContent += "#" + round + " You loose. Scissors cut PAPER!" + newLine
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         ++round
         ++playerScore;
         computerPaper.classList.add("played");
-        userScissors.classList.add("played");
         document.getElementById("user-points").textContent = playerScore;
         document.getElementById("round-result").textContent += "#" + round + " You win. SCISSORS cut paper!" + newLine
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         ++round
         ++computerScore;
         computerRock.classList.add("played");
-        userScissors.classList.add("played");
         document.getElementById("computer-points").textContent = computerScore;
         document.getElementById("round-result").textContent += "#" + round + " You loose. Rock crushes SCISSORS!" + newLine
     } else console.log("Is your spelling correct? Choose rock, paper or scissors.");
@@ -99,24 +108,27 @@ function playRound(playerSelection, computerSelection) {
 }
 
 document.querySelector("#rock-button").addEventListener("click", function() {
-    removeTransition()
     if (gameIsActive === true) {
         playerSelection = "rock"
-        return playRound(playerSelection, computerSelection);
+        userRock.classList.add("played");
+        playRound(playerSelection, computerSelection);
+        return removeTransitionRock();
 }});
 
 document.querySelector("#paper-button").addEventListener("click", function() {
-    removeTransition() 
     if (gameIsActive === true) {
-    playerSelection = "paper"
-    return playRound(playerSelection, computerSelection);
+        playerSelection = "paper"
+        userPaper.classList.add("played");
+        playRound(playerSelection, computerSelection);
+        return removeTransitionPaper();
 }});
 
 document.querySelector("#scissors-button").addEventListener("click", function() {
-    removeTransition()
     if (gameIsActive === true) {
-    playerSelection = "scissors"
-    return playRound(playerSelection, computerSelection);
+        playerSelection = "scissors"
+        userScissors.classList.add("played");
+        playRound(playerSelection, computerSelection);
+        return removeTransitionScissors();
 }});
 
 
